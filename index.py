ba551731fc52d06efbfa2b2b75a5c8c996e42870
Xuan <leeyitsuen0617@gmail.com>
2023-11-21 17:11:30 +0800

    modify

--- index.py
+++ index.py
@@ -2,14 +2,18 @@ import tkinter as tk
 from tkinter import ttk
 import pandas as pd
 import sqlite3
-
+from tkinter.simpledialog import Dialog
 
 class Window(tk.Tk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.title("信用卡消費樣態")
         self.conn = sqlite3.connect("creditcard.db")
+
         # -----------interface-----------#
+        mainFrame = tk.Frame(self, relief=tk.GROOVE, borderwidth=1)
+        tk.Label(mainFrame, text='信用卡消費樣態', font=('arial', 20), fg='#333333', padx=10, pady=10).pack(padx=20, pady=20, fill='both')
+        mainFrame.pack(padx=30, pady=10, fill='both')
 
         # ------搜尋------#
         topFrame = ttk.Labelframe(self, text="搜尋")
@@ -79,6 +83,7 @@ class Window(tk.Tk):
                 "10",
                 "11",
                 "12",
+                "ALL"
             ],
         )
         self.month.grid(row=0, column=5, padx=10, pady=10)
@@ -112,6 +117,7 @@ class Window(tk.Tk):
                 "新竹市",
                 "金門縣",
                 "連江縣",
+                "ALL"
             ],
         )
         self.area.grid(row=0, column=7, padx=10, pady=10)
@@ -122,12 +128,13 @@ class Window(tk.Tk):
         self.industry = ttk.Combobox(
             topFrame,
             textvariable=self.industry_var,
-            values=["食", "衣", "住", "行", "文教康樂", "百貨", "其他"],
+            values=["食", "衣", "住", "行", "文教康樂", "百貨", "其他", "ALL"],
         )
         self.industry.grid(row=0, column=9, padx=10, pady=10)
         self.industry.bind("<<ComboboxSelected>>", self.load_industry)
 
-        topFrame.pack()
+        topFrame.pack(padx=30, pady=10, fill='both')
+
         # ------------資料呈現------------#
         middleFrame = ttk.Labelframe(self, text="資料")
         self.treeview = ttk.Treeview(middleFrame, show="headings", height=20)
@@ -156,29 +163,22 @@ class Window(tk.Tk):
         tk.Label(bottomFrame3, text="").pack()
         bottomFrame3.pack()
 
+        # Bind the event after creating self.treeview
+        self.treeview.bind("<ButtonRelease-1>", self.selectedItem)
+
     def load_data(self, event):
-        selected_data = self.data_var.get()
-        print(f"Data:{selected_data}")
         self.load_treeview()
 
     def load_year(self, event):
-        selected_year = self.year_var.get()
-        print(f"Year:{selected_year}")
         self.load_treeview()
 
     def load_month(self, event):
-        selected_month = self.month_var.get()
-        print(f"Month:{selected_month}")
         self.load_treeview()
 
     def load_area(self, event):
-        selected_area = self.area_var.get()
-        print(f"Area:{selected_area}")
         self.load_treeview()
 
     def load_industry(self, event):
-        selected_industry = self.industry_var.get()
-        print(f"Industry:{selected_industry}")
         self.load_treeview()
 
     def load_treeview(self):
@@ -191,17 +191,27 @@ class Window(tk.Tk):
         if selected_option and selected_year:
             sql = f"SELECT * FROM {selected_option} WHERE 年 = '{selected_year}'"
 
-            if selected_month and selected_month != "Select Month":
+            if selected_month and selected_month != "Select Month" and selected_month != "ALL":
                 sql += f" AND 月 = '{selected_month}'"
 
-            if selected_area and selected_area != "Select Area":
+            if selected_area and selected_area != "Select Area" and selected_area != "ALL":
                 sql += f" AND 地區 = '{selected_area}'"
 
-            if selected_industry and selected_industry != "Select Industry":
+            if selected_industry and selected_industry != "Select Industry" and selected_industry != "ALL":
                 sql += f" AND 產業別 = '{selected_industry}'"
 
             data = pd.read_sql_query(sql, self.conn)
             self.display_data(data)
+
+
+    def selectedItem(self, event):
+        selected_item = self.treeview.focus()
+        data_dict = self.treeview.item(selected_item)
+        data_list = data_dict["values"]
+        if data_list:
+            index = self.treeview.index(selected_item)
+             # 將欄位名稱和資料一起傳遞給 ShowDetail
+            ShowDetail(self, self.treeview['columns'], data_list, title="資訊")
 
     def display_data(self, data):
         self.treeview.delete(*self.treeview.get_children())  # 清空 Treeview
@@ -217,11 +227,36 @@ class Window(tk.Tk):
                 values = [row[col] for col in columns]
                 self.treeview.insert("", "end", values=values)
 
-    
+class ShowDetail(Dialog):
+    def __init__(self, parent, columns, data, **kwargs):
+        self.columns = columns
+        self.data = data
+        super().__init__(parent, **kwargs)
+
+    def body(self, master):
+
+        try:
+            for col, value in zip(self.columns, self.data):
+                dataInfo = tk.Label(master, text=f"{col}:  {value}")
+                dataInfo.pack(pady=1, anchor='nw')
+        except Exception as e:
+            print(f"Exception in ShowDetail: {e}")
+            print("Columns:", self.columns)
+            print("Data:", self.data)
+
+    def buttonbox(self):
+        box = tk.Frame(self)
+        w = tk.Button(box, text="確認", width=10, command=self.ok, default="active")
+        w.pack(padx=5, pady=(5, 20))
+        self.bind("<Return>", self.ok)
+
+        box.pack()
+
 
 
 def main():
     window = Window()
+    window.resizable(width=False,height=False)
     window.mainloop()
 
 
