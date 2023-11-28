d5295aaa6c087f2bdb054d2b5881e9b090ff2ad8
Xuan <leeyitsuen0617@gmail.com>
2023-11-28 16:06:10 +0800

    modify

--- data.py
+++ data.py
@@ -4,7 +4,11 @@ import csv
 import sqlite3
 import pandas as pd
 
+__all__=['csv_to_database']
 
+'''
+執行前,需先建立datasource資料夾,底下建立age/incom/education/job/sex資料夾
+'''
 # ---------下載資料---------#
 def __download_credit_data() -> csv:
     area = [
@@ -62,23 +66,22 @@ def __download_credit_data() -> csv:
     industry = ["FD", "CT", "LG", "TR", "EE", "DP", "X2", "OT", " IDSUM", "ALL"]
     DataType = ["sex", "job", "incom", "education", "age"]
     sex = ["M", "F"]
-    sex_code = {"1":"男性", "2":"女性"}
+    sex_code = {"1": "男性", "2": "女性"}
 
-    # 兩性消費
+    #---兩性消費---#
     for A in industry:
         for B in area:
             sex_url = (
                 f"https://bas.nccc.com.tw/nccc-nop/OpenAPI/C01/sexconsumption/{B}/{A}"
             )
             response_sex = requests.request("GET", sex_url)
-            if len(response_sex.text) == 0:
+            if len(response_sex.text) == 0:   #若csv無資料則繼續,不下載
                 continue
             with open(f"./datasource/sex/sex{B}_{A}.csv", "wb") as file:
                 file.write(response_sex.content)
-                file.close()
     print("性別消費資料讀取成功")
 
-    # 各職業類別消費樣態資料
+    #---各職業類別消費---#
     for E in industry:
         for F in area:
             job_url = (
@@ -89,10 +92,9 @@ def __download_credit_data() -> csv:
                 continue
             with open(f"./datasource/job/job{F}_{E}.csv", "wb") as file:
                 file.write(response_job.content)
-                file.close()
     print("職業類別消費資料讀取成功")
 
-    # 各年收入族群消費樣態資料(V)
+    #---各年收入族群消費---#
     for G in industry:
         for H in area:
             incom_url = f"https://bas.nccc.com.tw/nccc-nop/OpenAPI/C03/incomegroupsconsumption/{H}/{G}"
@@ -101,10 +103,9 @@ def __download_credit_data() -> csv:
                 continue
             with open(f"./datasource/incom/incom{H}_{G}.csv", "wb") as file:
                 file.write(response_incom.content)
-                file.close()
     print("收入類別消費資料讀取成功")
 
-    # 各教育程度消費樣態資料(V)
+    #---各教育程度消費---#
     for I in industry:
         for J in area:
             education_url = f"https://bas.nccc.com.tw/nccc-nop/OpenAPI/C05/educationconsumption/{J}/{I}"
@@ -113,10 +114,9 @@ def __download_credit_data() -> csv:
                 continue
             with open(f"./datasource/education/education{J}_{I}.csv", "wb") as file:
                 file.write(response_education.content)
-                file.close()
     print("教育程度資料讀取成功")
 
-    # 兩性X各年齡層消費
+    #---兩性X各年齡層消費---#
     for A in industry:
         for B in area:
             for C in sex:
@@ -129,69 +129,67 @@ def __download_credit_data() -> csv:
                 file_path = os.path.join(folder_path, file_name)
                 with open(file_path, "wb") as file:
                     file.write(response_age.content)
-                    file.close()
     print("年齡層消費資料讀取成功")
 
     # ---------合併csv---------#
     for item in DataType:
-        path = f"./datasource/{item}/"
+        path = f"./datasource/{item}/"  #要合併資料的路徑位置
+        #取得指定路徑下所有的csv
         csv_files = [file for file in os.listdir(path) if file.endswith(".csv")]
+        #建立空dataframe儲存合併資料
         merged_data = pd.DataFrame()
+        #讀取csv資料內容合併到merged_data
         for file in csv_files:
-            file_path = os.path.join(path, file)
-            data = pd.read_csv(file_path)
-            merged_data = pd.concat([merged_data, data], ignore_index=True)
-        merged_data.to_csv(f"{item}.csv", index=False)
+            file_path = os.path.join(path, file)  #將路徑(path)與file連接,建立完整路徑位置
+            data = pd.read_csv(file_path)  #讀取csv內容,並儲存在dataframe(data)中
+            #pd.concat->將data與merged_data連接, ignore_index=True->忽略原始的index欄位(索引值),改採用合併後自動產生的index(索引值)
+            merged_data = pd.concat([merged_data, data], ignore_index=True) 
+        merged_data.drop_duplicates(inplace=True)  #在合併前去除重複的值
+        merged_data.to_csv(f"{item}.csv", index=False)  #將merged_data寫入csv, index=False->不寫入dataframe的index(索引值)
         print(f"{item}.csv建立成功")
 
         with open(f"./{item}.csv", "r", encoding="UTF-8") as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            fieldnames = csv_reader.fieldnames
+            csv_reader = csv.DictReader(csv_file)  #使用DictReader+csv_reader.fieldnames->傳回csv的欄位名稱
+            fieldnames = csv_reader.fieldnames  #取得欄位名稱
 
-            with open(f"./{item}_trans.csv", "w", encoding="utf-8", newline="") as file:
-                new_fieldnames = ["年", "月"] + fieldnames[1:]
+            #將格式轉換完畢的資料內容寫入csv中
+            with open(f"./{item}_trans.csv", "w", encoding="utf-8", newline="") as file:  
+                new_fieldnames = ["年", "月"] + fieldnames[1:]  #將年,月寫在欄位最前面, fieldnames[1:]->取得從2到最後的欄位名稱(原始fieldnames中年月是在第一個,所以從第二個開始)
                 csv_writer = csv.DictWriter(file, fieldnames=new_fieldnames)
-                csv_writer.writeheader()
+                csv_writer.writeheader()  #寫入第一列的欄位名稱
 
                 for row in csv_reader:
-                    # 使用get方法，如果找不到對應的鍵，就保持原來的值
+                    #---找不到對應的值，則保持原本的值---#
                     row["地區"] = area_code.get(row["地區"], row["地區"])
 
-                    if '性別' in fieldnames:
-                        row['性別'] = sex_code.get(row["性別"], row["性別"])
-                
-                    year = row["年月"][:4]
-                    month = row["年月"][4:]
+                    if "性別" in fieldnames:
+                        row["性別"] = sex_code.get(row["性別"], row["性別"])
+
+                    #將原資料中的年月,拆開成年 月,ex:201401
+                    year = row["年月"][:4]  #取出值,由開頭到3, 0-3, 2014
+                    month = row["年月"][4:] #取出值,由4到最後, 4-5, 01
                     new_row = {"年": year, "月": month, "地區": row["地區"]}
 
-                    if '性別' in fieldnames:
-                        new_row['性別'] = row['性別']
-                
-                    new_row.update(row)
-                    del new_row["年月"]
-                    csv_writer.writerow(new_row)
+                    if "性別" in fieldnames:  #找尋csv中有性別欄位的資料
+                        new_row["性別"] = row["性別"]
+
+                    new_row.update(row)  #將dictionary(new_row)更新並合併到另一個dictionary(row)
+                    del new_row["年月"]  #刪除原csv裡的年月
+                    csv_writer.writerow(new_row)  #將new_row資料寫入
 
                 print(f"{item}_trans.csv建立成功")
 
 
 # ---------輸入資料---------#
-def csv_to_database(conn: sqlite3.Connection) -> None:
+def csv_to_database() -> None:
+    __download_credit_data()
+    conn = sqlite3.connect("creditcard.db")
     DataType = ["sex", "job", "incom", "education", "age"]
     for item in DataType:
         file = f"./{item}_trans.csv"
         df = pd.read_csv(file)
 
-        df.rename(columns={"信用卡交易金額[新台幣]": "信用卡金額"}, inplace=True)
-        df.to_sql(item, conn, if_exists="append", index=False)
+        df.rename(columns={"信用卡交易金額[新台幣]": "信用卡金額"}, inplace=True)  #inplace=True->直接修改原始dataframe
+        df.to_sql(item, conn, if_exists="replace", index=False)  #寫入資料庫中,if_exists="replace"->若table已經存在將其替換, index=False->不將dataframe的索引值寫入
 
     conn.close()
-
-
-def main() -> None:
-    __download_credit_data()
-    conn = sqlite3.connect("creditcard.db")
-    csv_to_database(conn)
-
-
-if __name__ == "__main__":
-    main()
