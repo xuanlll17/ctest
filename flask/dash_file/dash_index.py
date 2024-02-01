e9ba4ffc6f3f601986097cf6be211f351de4f7a8
HH <wlhs1726@gmail.com>
2024-02-01 15:12:54 +0000

    modify

--- flask/dash_file/dash_index.py
+++ flask/dash_file/dash_index.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dash_table, callback, Input, Output, dcc
+from dash import Dash, html, callback, Input, Output, dcc
 import dash_bootstrap_components as dbc
 import pandas as pd
 from . import data
@@ -7,7 +7,7 @@ import plotly.express as px
 dash_index = Dash(
     requests_pathname_prefix="/dash/index/", external_stylesheets=[dbc.themes.QUARTZ]
 )
-dash_index.title = "信用卡消費樣態"
+dash_index.title = "2023信用卡消費樣態"
 lastest_data = data.search_data(dataName="年齡層",tableName="age")
 lastest_df = pd.DataFrame(
     lastest_data, columns=["年", "月", "地區", "產業別", "年齡層", "信用卡交易筆數", "信用卡交易金額"]
@@ -18,7 +18,7 @@ dash_index.layout = html.Div(
         dbc.Container(
             [
                 html.Div(
-                    [html.Div([html.H1("信用卡消費樣態")], className="col text-center")],
+                    [html.Div([html.H1("2023信用卡消費樣態")], className="col text-center")],
                     className="row",
                     style={"padding": "3rem 0 1rem 0"},
                 ),
@@ -27,11 +27,11 @@ dash_index.layout = html.Div(
                 ]),
                 html.Div(
                     [
-                        dbc.Button("教育程度", color="secondary", className="me-4", href="/dash/app/", external_link=True, style={"fontWeight": "bold"}),
+                        dbc.Button("教育程度", color="info", className="me-4", href="/dash/app/", external_link=True, style={"fontWeight": "bold"}),
                         dbc.Button("年齡層", color="secondary", className="me-4", href="/dash/app1/", external_link=True, style={"fontWeight": "bold"}),
-                        dbc.Button("職業類別", color="secondary", className="me-4", href="/dash/app2/", external_link=True, style={"fontWeight": "bold"}),
-                        dbc.Button("性別", color="secondary", className="me-4", href="/dash/app3/", external_link=True, style={"fontWeight": "bold"}),
-                        dbc.Button("年收入", color="secondary", className="me-4", href="/dash/app4/", external_link=True, style={"fontWeight": "bold"}),
+                        dbc.Button("職業類別", color="success", className="me-4", href="/dash/app2/", external_link=True, style={"fontWeight": "bold"}),
+                        dbc.Button("性別", color="warning", className="me-4", href="/dash/app3/", external_link=True, style={"fontWeight": "bold"}),
+                        dbc.Button("年收入", color="primary", className="me-4", href="/dash/app4/", external_link=True, style={"fontWeight": "bold"}),
                     ],
                     className="d-flex justify-content-center",
                     style={"paddingTop": "2.5rem"},
@@ -61,5 +61,3 @@ def line_chart(graph_id):
         xaxis={'showgrid': False, 'color':'white'},
         yaxis={'showgrid': True, 'color':'white'})
     return fig
-
-
